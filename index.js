0d0e5d7debfefaac3340bd8bbc4075460acf7480
HadiChan <15089581429@163.com>
2019-06-11 17:59:08 +0800

    add util

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var ddvRestFulApi = require('./api.js')
 var onAccessKey = require('./lib/onAccessKey.js')
 var request = require('./lib/request.js')
 var upload = require('./lib/upload/index.js')
+var util = require('ddv-restful-api/util')
 
 module.exports = ddvRestFulApi.getApi(function (api) {
   api.setHeadersPrefix('x-ddv-')
@@ -10,6 +11,7 @@ module.exports = ddvRestFulApi.getApi(function (api) {
   api.uploadApi = function (data) {
     return upload(data, api)
   }
+  api.util = Object.assign(api.util || {}, util)
   // eslint-disable-next-line no-undef
   if (typeof define !== 'undefined' && typeof requirejs !== 'undefined') {
     // eslint-disable-next-line no-undef
